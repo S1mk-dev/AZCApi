2cde748589ef12e56d67f7e8ec6f54fca1057d77
S1mk-dev <96042836+S1mk-dev@users.noreply.github.com>
2021-12-13 11:38:29 +0300

    Добавьте файлы проекта.

---
+++ AZCApi/Helpers/DataBaseHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZCApi
+{
+    public class DataBaseHelper : DbContext
+    {
+        public DbSet<Station> Station { get; set; }
+        public DbSet<Date> Date {get; set; }
+
+        public DataBaseHelper(DbContextOptions<DataBaseHelper> options)
+            : base(options)
+        {
+
+        }
+
+        public DataBaseHelper()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(@"Data Source = DESKTOP - 53P7MAE; Initial Catalog = CarFillingStation; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+    }
+}
